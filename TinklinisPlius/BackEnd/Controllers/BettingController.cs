4f64c103e26c02b885fe93ddb3e7f5688949f033
VytRib <vytenris@gmail.com>
2025-05-24 19:31:13 +0300

    active wagers crude

--- TinklinisPlius/BackEnd/Controllers/BettingController.cs
+++ TinklinisPlius/BackEnd/Controllers/BettingController.cs
@@ -47,7 +47,7 @@ public class BettingController : Controller
 
     /// Wager history (Admin)
     public IActionResult WagerHistoryWindow()
-{
+    {
     var finishedWagers = new List<Wager>();
     string connString = _configuration.GetConnectionString("DefaultConnection");
 
@@ -74,5 +74,42 @@ public class BettingController : Controller
     }
 
     return View("WagerHistoryWindow", finishedWagers);
+}
+
+    /// Ongoing wagers (User)
+    public IActionResult WagerInfoWindow()
+{
+    var ongoingWagers = new List<Wager>();
+    string connString = _configuration.GetConnectionString("DefaultConnection");
+
+    using (var conn = new NpgsqlConnection(connString))
+    {
+        conn.Open();
+
+        using (var cmd = new NpgsqlCommand(@"
+            SELECT w.* 
+            FROM Wager w
+            JOIN Match m ON w.fk_Matchid_Match = m.id_Match
+            WHERE 
+                w.hasFinished = FALSE
+                AND m.date > CURRENT_DATE", conn))
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                ongoingWagers.Add(new Wager
+                {
+                    IdWager = reader.GetInt32(reader.GetOrdinal("id_Wager")),
+                    Chance = reader.IsDBNull(reader.GetOrdinal("chance")) ? null : reader.GetInt32(reader.GetOrdinal("chance")),
+                    Combinedsum = reader.IsDBNull(reader.GetOrdinal("combinedSum")) ? null : reader.GetInt32(reader.GetOrdinal("combinedSum")),
+                    Hasfinished = reader.IsDBNull(reader.GetOrdinal("hasFinished")) ? null : reader.GetBoolean(reader.GetOrdinal("hasFinished")),
+                    FkMatchidMatch = reader.GetInt32(reader.GetOrdinal("fk_Matchid_Match")),
+                    FkMatchfkTournamentidTournament = reader.GetInt32(reader.GetOrdinal("fk_Matchfk_Tournamentid_Tournament"))
+                });
+            }
+        }
+    }
+
+    return View("WagerInfoWindow", ongoingWagers);
 }
 }
