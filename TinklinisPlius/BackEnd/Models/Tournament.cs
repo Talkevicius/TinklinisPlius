3ab9d3647473aeb06cd05080da5a9996ad96b916
Titas <titastalkevicius@gmail.com>
2025-05-26 20:36:02 +0300

    forced to input all fields in tournament

--- TinklinisPlius/BackEnd/Models/Tournament.cs
+++ TinklinisPlius/BackEnd/Models/Tournament.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TinklinisPlius.Models;
 
 public class Tournament
 {
     public int IdTournament { get; set; }
-
+    [Required(ErrorMessage = "Title is required")]
     public string? Title { get; set; }
 
+    [Required(ErrorMessage = "Start date is required")]
     public DateOnly? Startdate { get; set; }
 
+    [Required(ErrorMessage = "End date is required")]
     public DateOnly? Enddate { get; set; }
 
+    [Required(ErrorMessage = "Country is required")]
     public string? Country { get; set; }
 
     public int? Teamnr { get; set; }
